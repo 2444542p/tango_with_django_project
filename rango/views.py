ee0b50b001b73a4c8e29a26f8e7c26efddd192bd
2444542p <2444542p@student.gla.ac.uk>
2021-02-05 13:32:28 +0200

    9.11 Exercises

--- rango/views.py
+++ rango/views.py
@@ -51,6 +51,7 @@ def show_category(request, category_name_slug):
     
     return render(request,'rango/category.html', context=context_dict)
 
+@login_required
 def add_category(request):
     form = CategoryForm()
 
@@ -69,6 +70,7 @@ def add_category(request):
     #Render the form with error messages (if any).
     return render(request, 'rango/add_category.html', {'form':form})
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category=Category.objects.get(slug=category_name_slug)
@@ -154,7 +156,8 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    #return HttpResponse("Since you're logged in, you can see this text!")
+    return redirect(reverse('rango:restricted'))
 
 @login_required
 def user_logout(request):
